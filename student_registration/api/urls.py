c2df1e4a6d0fe5fc5e612abd5268555b74420ecc
Deepak Singh Adhikari <dpksingh.adhikari@gmail.com>
2023-08-15 21:42:51 +0530

    add more urls

--- student_registration/api/urls.py
+++ student_registration/api/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from classes.views import ClassCreateView
 from Students.views import StudentDetailView,StudentListCreateView
+from accounts.views import RegisterView,LoginView
 
 urlpatterns = [
+    path('register/',RegisterView.as_view()),
+    path('login/',LoginView.as_view()),
+
+
     path('class/',ClassCreateView.as_view()),
     path('studentregister/',StudentDetailView.as_view()),
     path('studentupdate/',StudentListCreateView.as_view()),
